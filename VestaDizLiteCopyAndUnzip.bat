03c617b5e42e996712aaae10b504ad2e1d8540ec
Alexey Vasilyev <bestspajic@gmail.com>
2013-12-26 21:18:16 +0400

    add example vesta installation .bat srcipt

---
+++ VestaDizLiteCopyAndUnzip.bat
@@ -0,0 +1,7 @@
+@echo off
+rmdir /S /Q VestaDizLite
+mkdir VestaDizLite
+cd VestaDizLite
+copy "C:\Users\1\Dropbox\VestaLite (2)\VestaDizLite.zip" VestaDizLite.zip
+"C:\Program Files\HaoZip\HaoZipC.exe" e VestaDizLite.zip
+VestaDizLite.exe /silent
