38737147a5f8b2d6c5595f119e01800a1d68c6d5
Alexey Vasilyev <bestspajic@gmail.com>
2014-01-08 19:54:03 +0400

    Hide installer with /verysilent key. Resolve #7.

--- VestaDizLiteCopyAndUnzip.bat
+++ VestaDizLiteCopyAndUnzip.bat
@@ -4,4 +4,4 @@ mkdir VestaDizLite
 cd VestaDizLite
 copy "C:\Users\1\Dropbox\VestaLite (2)\VestaDizLite.zip" VestaDizLite.zip
 "C:\Program Files\HaoZip\HaoZipC.exe" e VestaDizLite.zip
-VestaDizLite.exe /silent
+VestaDizLite.exe /verysilent
